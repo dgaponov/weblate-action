6cd5e3253cf59232b2ba2c3a3b277ffbd0014eba
Daniil Gaponov <dagaponov@yandex-team.ru>
2023-11-12 14:33:11 +0300

    feat: update version

--- src/config.ts
+++ src/config.ts
@@ -25,8 +25,8 @@ export function getConfiguration(): Configuration {
         throw Error('Weblate-action works only with pull requests');
     }
 
-    if (!context.payload.repository?.html_url) {
-        throw Error('Repository url not found');
+    if (!context.payload.repository?.ssh_url) {
+        throw Error('Repository ssh url not found');
     }
 
     return {
@@ -35,7 +35,7 @@ export function getConfiguration(): Configuration {
         project: getInput('PROJECT'),
         branchName: getBranchName(),
         fileFormat: getInput('FILE_FORMAT'),
-        gitRepo: context.payload.repository.html_url,
+        gitRepo: context.payload.repository.ssh_url,
         pullRequestNumber: context.payload.pull_request.number,
         keysetsPath: getInput('KEYSETS_PATH'),
     };
